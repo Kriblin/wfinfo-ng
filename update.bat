f0ed2d2e1d371780f604fdca17d1a075c6bb3221
Kriblin <nikogs95@gmail.com>
2026-07-10 00:43:35 +0200

    chore: Simplify external update scripts

--- update.bat
+++ update.bat
@@ -1,5 +1,3 @@
 @echo off
-curl https://api.warframestat.us/wfinfo/prices/ | jq . > prices.json
-curl https://api.warframestat.us/wfinfo/filtered_items/ | jq . > filtered_items.json
 if not exist tessdata mkdir tessdata
 curl -L https://github.com/tesseract-ocr/tessdata_fast/raw/main/eng.traineddata -o tessdata/eng.traineddata
