45624ff3db5c757d4b59743fad23a4705034d3c1
Kriblin <nikogs95@gmail.com>
2026-07-11 09:48:29 +0200

    chore: Update binary entry points to new module structure

--- src/bin/settings.rs
+++ src/bin/settings.rs
@@ -1,17 +1,3 @@
-use std::error::Error;
-
-use wfinfo::settings::SettingsApp;
-
-pub fn run_settings() -> Result<(), Box<dyn Error>> {
-    let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "WFinfo-ng Settings",
-        options,
-        Box::new(|_cc| Ok(Box::new(SettingsApp::new()))),
-    )
-    .map_err(|e| Box::new(e) as Box<dyn Error>)
-}
-
-fn main() -> Result<(), Box<dyn Error>> {
-    run_settings()
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    wfinfo::app::settings::run()
 }
